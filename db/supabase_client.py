705fcaa334ba23f7c98a4f6dcbbfacbb6c04408c
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-02-19 21:26:10 -0600

    Create supabase_client.py

---
+++ db/supabase_client.py
@@ -0,0 +1,17 @@
+#supabse_client.py
+
+from supabase import create_client, Client
+import streamlit as st
+
+@st.cache_resource
+
+def init_connection():
+    url = st.secrets["supabase_url"]
+    key = st.secrets["supabase_key"]
+    return create_client(url,key)
+
+supabase = init_connection()
+
+def upload_embedding(filename: str, filetype: str, embedding):
+    data = {"filename": filename, "filetype": filetype, "embedding": embedding}
+    supabase.table("embeddings").insert(data).execute()
